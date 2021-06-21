b5daf317ac7c585fd941d8cb17b1a154aaf76250
LeoKuD <then7w@gmail.com>
2021-06-21 17:36:50 +0300

    lesson 77, validation

--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -1,27 +1,49 @@
 import React from 'react'
-import { useForm } from "react-hook-form";
+import { useForm, Controller } from "react-hook-form";
+import { Input } from '../elementsUI/TextField';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControlLabel from "@material-ui/core/FormControlLabel"
+import Checkbox from "@material-ui/core/Checkbox";
+import { withStyles } from '@material-ui/core/styles';
+
+
 
 
 export const LoginForm = (props) => {
-    const { register, handleSubmit } = useForm()
+    const { control, register, handleSubmit, formState: { errors } } = useForm({
+        // defaultValues: {
+        //     isRemember: false
+        // },
+        mode: 'onBlur'
+    })
     const onSubmit = (data) => {
         console.log(data);
     }
 
+
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
-            <div>
-                <label htmlFor="email">Email</label>
-                <input placeholder='Email' {...register('email')} type='email'></input>
-            </div>
-            <div>
-                <label htmlFor="password">Password</label>
-                <input placeholder='Password' {...register('password')}></input>
-            </div>
-            <div>
-                <label htmlFor='isRemember'>Remember password</label>
-                <input type='checkbox' {...register('isRemember')} ></input>
-            </div>
+
+            <InputLabel htmlFor="email">login</InputLabel>
+            <Input placeholder='Введите логин' {...register('login', { validate: (value) => value.length > 5 })} type='text'></Input>
+            {errors.login && <p>This is required and is less than 5 characters</p>}
+
+
+            <InputLabel htmlFor="password">Password</InputLabel>
+            <Input placeholder='Введите пароль' {...register('password', { validate: (value) => value.length > 8 })}></Input>
+            {errors.password && <p>This is required and is less than 8 characters</p>}
+
+            <FormControlLabel
+                control={
+                    <Controller
+                        name="isRemember"
+                        control={control}
+                        defaultValue={false}
+                        render={({ field }) => <Checkbox {...field} />}
+                    />
+                }
+                label="Remember"
+            />
             <input type="submit" />
         </form>
     )
--- src/components/Profile/Myposts/MyPostsForm.jsx
+++ src/components/Profile/Myposts/MyPostsForm.jsx
@@ -1,21 +1,43 @@
 import React from 'react'
 import { useForm } from 'react-hook-form'
+import { Input } from '../../elementsUI/TextField'
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import InputLabel from '@material-ui/core/InputLabel';
+
+
+const schema = yup.object().shape({
+    yourPost: yup
+        .string().max(30, 'Вводите не больше 30 символов')
+        .required('Введите сообщение')
+
+})
+
+
 
 export const MyPostsForm = (props) => {
 
 
-    const { register, handleSubmit } = useForm()
+    const { register, handleSubmit, formState: { errors } } = useForm({
+        resolver: yupResolver(schema),
+    })
     const onSubmit = (data) => {
         console.log(data);
         props.addPost(data.yourPost)
+
+
     }
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
-            <label htmlFor="yourPost">Ваш пост</label>
-            <input {...register('yourPost')}
+            <InputLabel htmlFor="yourPost">Ваш пост</InputLabel>
+            <Input {...register('yourPost')}
                 name='yourPost'
+                id='yourPost'
                 type="text"
-                placeholder={'your post'} />
+                placeholder={'Введите текст вашего поста'}
+                error={!!errors.yourPost}
+                helperText={errors?.yourPost?.message}
+            />
 
             <input type="submit" name="addPost" id="" />
         </form>
